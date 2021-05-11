22a60f9f8cd78cc3a57126fb5061270b1fb5cf6a
Franjo Topić <ftopic@racunarstvo.hr>
2021-05-11 21:43:17 +0200

    Test

--- WebAPI/Controllers/BookController.cs
+++ WebAPI/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//Test komentar za github
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI.Controllers
