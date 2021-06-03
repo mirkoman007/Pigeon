adbe890d51a90b80ac3b418bef78529cf57e95be
mirkoman007 <mirkozaper2@gmail.com>
2021-06-03 19:13:36 +0200

    Login (returnUrl) fix

--- WebApplication/Helpers/AppRouteView.cs
+++ WebApplication/Helpers/AppRouteView.cs
@@ -24,7 +24,7 @@ namespace WebApplication.Helpers
             if (authorize && AuthenticationService.User == null)
             {
                 var returnUrl = WebUtility.UrlEncode(new Uri(NavigationManager.Uri).PathAndQuery);
-                NavigationManager.NavigateTo($"login?returnUrl={returnUrl}");
+                NavigationManager.NavigateTo($"login?returnUrl={returnUrl}",true);
             }
             else
             {
