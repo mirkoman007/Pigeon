686c8a2719773b30c814cba4a63a0e6542c80c29
mirkoman007 <mirkozaper2@gmail.com>
2021-07-10 19:00:58 +0200

    Friend request

---
+++ WebApplication/Models/Friends/FriendRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models.Friends
+{
+    public class FriendRequest
+    {
+        public int IdfriendRequest { get; set; }
+        public int UserRequestId { get; set; }
+        public string UserRequestName { get; set; }
+        public int UserResponderId { get; set; }
+        public string UserResponderName { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
--- WebApplication/Services/FriendService.cs
+++ WebApplication/Services/FriendService.cs
@@ -8,7 +8,12 @@ namespace WebApplication.Services
 {
     public interface IFriendService
     {
+        Task SendFriendRequest (int userRequestID, int userRespondID);
+        Task<IList<FriendRequest>> GetFriendRequestSent(int id);
+        Task<IList<FriendRequest>> GetFriendRequestReceived(int id);
         Task<IList<Friend>> GetAllFriends(int id);
+        Task<String> AcceptFriendRequest(int userRequestID, int userRespondID);
+        Task<String> DeclineFriendRequest(int userRequestID, int userRespondID);
 
     }
 
@@ -21,9 +26,34 @@ namespace WebApplication.Services
             _httpService = httpService;
         }
 
+        public async Task SendFriendRequest(int userRequestID, int userRespondID)
+        {
+            await _httpService.Post($"/api/Friends/request/send/{userRequestID}/{userRespondID}",null);
+        }
+
+        public async Task<IList<FriendRequest>> GetFriendRequestReceived(int id)
+        {
+            return await _httpService.Get<IList<FriendRequest>>($"/api/Friends/request/received/{id}");
+        }
+
+        public async Task<IList<FriendRequest>> GetFriendRequestSent(int id)
+        {
+            return await _httpService.Get<IList<FriendRequest>>($"/api/Friends/request/sent/{id}");
+        }
+
         public async Task<IList<Friend>> GetAllFriends(int id)
         {
             return await _httpService.Get<IList<Friend>>($"/api/Friends/all/{id}");
+        }
+
+        public async Task<String> AcceptFriendRequest(int userRequestID, int userRespondID)
+        {
+            return await _httpService.Get<String>($"/api/Friends/accept/{userRequestID}/{userRespondID}");
+        }
+
+        public async Task<String> DeclineFriendRequest(int userRequestID, int userRespondID)
+        {
+            return await _httpService.Get<String>($"/api/Friends/decline/{userRequestID}/{userRespondID}");
         }
     }
 }
